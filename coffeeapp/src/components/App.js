15a0c80b0fb7851b5c1432bc67481ec4a9af2c3a
hernan verar <hernanverar@gmail.com>
2023-09-15 11:25:16 -0700

    WIP doing research about Routes

--- coffeeapp/src/components/App.js
+++ coffeeapp/src/components/App.js
@@ -1,14 +1,18 @@
-import Header from './Header';
+// import Header from './Header';
 import './App.css';
-import {BrowserRouter as Router, } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div>
-      <Header />
-    </div>
+  return <div className='App'>
 
-  )
+    <Router>
+      <Routes>
+        <Route path="/" />
+        <Route path="/cart" />
+      </Routes>
+      </Router>
+    
+    </div>
 }
 
 export default App;
