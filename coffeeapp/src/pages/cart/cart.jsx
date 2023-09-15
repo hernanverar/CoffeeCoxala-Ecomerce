1b310daee829403196a8b5797a0aa874785953dc
hernan verar <hernanverar@gmail.com>
2023-09-15 16:20:02 -0700

    This commit adds shop and cart files is where our items are gooing to live in the future

---
+++ coffeeapp/src/pages/cart/cart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Cart = () => {
+  return (
+    <div><h2>This are my orders</h2></div>
+  )
+}
---
+++ coffeeapp/src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div className='shop'> 
+      <div className='title'>
+        <h1>Coxala Coffee Shop</h1>
+      </div>
+      <div className='Products'></div>
+    </div>
+  )
+}
+
+
