d1576a507d6313225e7e0bdfd914df22ed3c8972
hernan verar <hernanverar@gmail.com>
2023-09-29 13:54:04 -0700

    This is a WIP on how to link and redirect full menu to my button

--- coffeeapp/src/pages/Home.js
+++ coffeeapp/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,11 +7,18 @@ import Salad from "../assets/coxala-pics/Salad.jpeg";
 import Expresso from "../assets/coxala-pics/Expresso.jpeg";
 import cookieDrink from "../assets/coxala-pics/cookieDrink.jpeg";
 import GingerBeer from "../assets/coxala-pics/GingerBeer.jpeg";
+import MenuCoxala from "../assets/coxala-pics/MenuCoxala.jpeg";
 import Flan from "../assets/coxala-pics/Flan.jpeg";
 import { Link } from "react-router-dom";
 import "../pages/styles/Home.css"
 
 function Home() {
+  const sliderRef = useRef(null);
+
+  const goToHours = () => {
+    sliderRef.current.slickGoTo(0); // Go to the index of the "Hours" image
+  };
+
   const settings = {
     infinite: true,
     dots: true,
@@ -25,9 +32,14 @@ function Home() {
       <div className="headerContainer">
         <h1>Welcome to Coffee Coxala!</h1>
         <Link to="/menu">
-          <button className="redirect">check our full Menu</button>
+          <button className="redirect" onClick={goToHours}>
+            check our full Menu
+          </button>
         </Link>
-        <Slider {...settings}>
+        
+
+        <Slider ref={sliderRef} {...settings}>
+        <img src={MenuCoxala} alt="Menu of Coxala" className="menu-img" />
           <img src={Bagels} alt="Bagels" />
           <img src={cookieDrink} alt="cookieDrink" />
           <img src={GingerBeer} alt="GingerBeer" />
