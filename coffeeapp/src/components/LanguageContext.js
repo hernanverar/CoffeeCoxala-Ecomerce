4662446c19152602228569af4049cb82d3ed600e
hernan verar <hernanverar@gmail.com>
2023-09-29 11:46:38 -0700

    This commit adds new file to allow our project to change lenguages

---
+++ coffeeapp/src/components/LanguageContext.js
@@ -0,0 +1,21 @@
+// LanguageContext.js
+import React, { createContext, useState } from "react";
+
+const LanguageContext = createContext();
+
+const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState("en");
+
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) => (prevLanguage === "en" ? "es" : "en"));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export { LanguageProvider, LanguageContext };
+;
--- coffeeapp/src/pages/Menu.js
+++ coffeeapp/src/pages/Menu.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LanguageContext } from "../components/LanguageContext";
 import { MenuList } from "../helpers/MenuList";
 import MenuItem from "../components/MenuItem";
-import "../pages/styles/Menu.css"
+import "../pages/styles/Menu.css";
 
+const Menu = () => {
+  const { language } = useContext(LanguageContext);
 
+  const content = {
+    en: "English Content",
+    es: "Contenido en español",
+  };
 
-function Menu() {
   return (
-    <div className="menu">
-      <h1 className="menuTitle">Our Menu</h1>
-      <div className="menuList">
-        {MenuList.map((menuItem, key) => {
-          return (
+    <div>
+      {content[language]}
+      <div className="menu">
+        <h1>{language === "en" ? "Menu" : "Menú"}</h1>
+        <div className="menuList">
+          {MenuList.map((menuItem, key) => (
             <MenuItem
-            key={key}
-            image={menuItem.image}
-            name={menuItem.name}
-            price={menuItem.price} />
-          );
-        })}
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              price={menuItem.price}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Menu;
