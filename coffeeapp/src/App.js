1b8a96ca05be5818231927465e88ea7a125cc244
hernan verar <hernanverar@gmail.com>
2023-10-06 13:37:22 -0700

    This commit updates Navbar.css adds CartItem.js

--- coffeeapp/src/App.js
+++ coffeeapp/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Footer } from "./components/Footer";
-// import Cart from "./components/Cart";
+// import CartItem from "./components/CartItem";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import About from "./pages/About";
@@ -12,6 +12,11 @@ import Contact from "./pages/Contact";
 import Crud from "./pages/Crud";
 
 function App() {
+
+
+  // const [{ cart, total }, dispatch] = useStateValue();
+  // const [totalPrice, setTotalPrice] = useState(0);
+
   return (
     <div className="photo">
     <div className="App">
--- coffeeapp/src/components/CartItem.js
+++ coffeeapp/src/components/CartItem.js
@@ -1,10 +1,12 @@
-// import { AddRounded, RemoveRounded } from "@mui/icons-material";
+
 // import React, { useEffect, useState } from "react";
-// import { MenuList} from "./helpers/MenuList";
 // import { useStateValue } from "./StateProvider";
+
+
 // let cartItems = [];
 
-// function Cart({name, price}) {
+// function CartItem({ price }) {
+//   const [qty, setQty] = useState(1);
 //   const [itemPrice, setItemPrice] = useState(parseInt(qty) * parseFloat(price));
 //   const [{ cart, total }, dispatch] = useStateValue();
 
@@ -17,7 +19,6 @@
 //     if (action == "add") {
 //       setQty(qty + 1);
 //     } else {
-   
 //       if (qty == 1) {
 //         cartItems.pop(id);
 //         dispatch({
@@ -32,23 +33,7 @@
 //   };
 
 //   return (
-//     <div>
-//     <div className="itemSection">
-//     <h2 className="itemName">{name}</h2>
-//     <div className="itemQuantity">
-//       <span>x {qty}</span>
-//       <div className="quantity">
-//         <RemoveRounded
-//           className="itemRemove"
-//           onClick={() => updateQty("remove", itemId)}
-//         />
-//         <AddRounded
-//           className="itemAdd"
-//           onClick={() => updateQty("add", itemId)}
-//         />
-//       </div>
-//     </div>
-//   </div>
+// <div>
 //   <p className="itemPrice">
 //     <span className="dolorSign">$</span>{" "}
 //     <span className="itemPriceValue">{itemPrice}</span>
@@ -57,4 +42,4 @@
 //   )
 // }
 
-// export default Cart
+// export default CartItem;
